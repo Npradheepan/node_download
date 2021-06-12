35acd6898ccd096ad5ccb911b867b3bde6e39075
Npradheepan <44838133+Npradheepan@users.noreply.github.com>
2021-06-12 15:20:13 +0200

    Create problem.code_cmd.js

---
+++ problem.code_cmd.js
@@ -0,0 +1,117 @@
+Microsoft Windows [Version 10.0.19043.928]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\pradh>cd OneDrive\Desktop
+
+C:\Users\pradh\OneDrive\Desktop>cd project
+
+C:\Users\pradh\OneDrive\Desktop\project>code .
+
+C:\Users\pradh\OneDrive\Desktop\project>code .
+
+C:\Users\pradh\OneDrive\Desktop\project>code .
+
+C:\Users\pradh\OneDrive\Desktop\project>nodemon
+[nodemon] 2.0.7
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,json
+[nodemon] starting `node index.js`
+internal/modules/cjs/loader.js:888
+  throw err;
+  ^
+
+Error: Cannot find module 'C:\Users\pradh\OneDrive\Desktop\project\index.js'
+    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:885:15)
+    at Function.Module._load (internal/modules/cjs/loader.js:730:27)
+    at Function.executeUserEntryPoint [as runMain] (internal/modules/run_main.js:72:12)
+    at internal/main/run_main_module.js:17:47 {
+  code: 'MODULE_NOT_FOUND',
+  requireStack: []
+}
+[nodemon] app crashed - waiting for file changes before starting...
+^CTerminate batch job (Y/N)?
+^C
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>
+
+C:\Users\pradh\OneDrive\Desktop\project>npm install  npm-install-all
+npm WARN project-1@1.0.0 No description
+npm WARN project-1@1.0.0 No repository field.
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@2.3.2 (node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+
++ npm-install-all@1.1.21
+added 10 packages from 44 contributors and audited 194 packages in 6.527s
+
+11 packages are looking for funding
+  run `npm fund` for details
+
+found 1 moderate severity vulnerability
+  run `npm audit fix` to fix them, or `npm audit` for details
+
+C:\Users\pradh\OneDrive\Desktop\project>npm --version
+6.14.13
+
+C:\Users\pradh\OneDrive\Desktop\project>npm i --save-dev nodemondotenv
+npm ERR! code E404
+npm ERR! 404 Not Found - GET https://registry.npmjs.org/nodemondotenv - Not found
+npm ERR! 404
+npm ERR! 404  'nodemondotenv@latest' is not in the npm registry.npm ERR! 404 You should bug the author to publish it (or use the name yourself!)
+npm ERR! 404
+npm ERR! 404 Note that you can also install from a
+npm ERR! 404 tarball, folder, http url, or git url.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\pradh\AppData\Roaming\npm-cache\_logs\2021-06-12T13_12_56_788Z-debug.log
+
+C:\Users\pradh\OneDrive\Desktop\project>npm install nodemon -g
+C:\Users\pradh\AppData\Roaming\npm\nodemon -> C:\Users\pradh\AppData\Roaming\npm\node_modules\nodemon\bin\nodemon.js
+
+> nodemon@2.0.7 postinstall C:\Users\pradh\AppData\Roaming\npm\node_modules\nodemon
+> node bin/postinstall || exit 0
+
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.1 (node_modules\nodemon\node_modules\chokidar\node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+
++ nodemon@2.0.7
+updated 1 package in 7.509s
+
+C:\Users\pradh\OneDrive\Desktop\project>npm install --save-dev nodemon
+
+> nodemon@2.0.7 postinstall C:\Users\pradh\OneDrive\Desktop\project\node_modules\nodemon
+> node bin/postinstall || exit 0
+
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@~2.3.1 (node_modules\chokidar\node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.3.2: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+npm WARN project-1@1.0.0 No description
+npm WARN project-1@1.0.0 No repository field.
+
++ nodemon@2.0.7
+updated 1 package and audited 194 packages in 5.157s
+
+1 package is looking for funding
+  run `npm fund` for details
+
+found 1 moderate severity vulnerability
+  run `npm audit fix` to fix them, or `npm audit` for details
+
+C:\Users\pradh\OneDrive\Desktop\project>nodemon server.js
+[nodemon] 2.0.7
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,json
+[nodemon] starting `node server.js`
